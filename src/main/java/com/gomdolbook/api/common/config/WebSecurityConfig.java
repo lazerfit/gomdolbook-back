3a5c363d4813e8d3733c5203975d706c4455625b
sg kim <redkafe98@gmail.com>
2025-08-25 17:12:46 +0900

    fix: webSecurityConfig

--- src/main/java/com/gomdolbook/api/common/config/WebSecurityConfig.java
+++ src/main/java/com/gomdolbook/api/common/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
